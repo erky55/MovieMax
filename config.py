3617a000eca5cb03eacf45b3e114f210fb4d7d5e
erky55 <142507623+erky55@users.noreply.github.com>
2024-03-13 17:55:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 from decouple import config
 
 BOT_TOKEN = config("BOT_TOKEN", default="")
+
+APP_ICON = config("APP_ICON", default="https://f004.backblazeb2.com/file/switch-bucket/e78c287a-abc0-11ee-883b-d41b81d4a9ef.png")
+SECONDARY_ICON = config("SECONDARY_ICON", default="https://f004.backblazeb2.com/file/switch-bucket/6c0928da-abc3-11ee-82f3-d41b81d4a9ef.png")
+
+ADS_TOKEN = config("ADS_TOKEN", default="")
+
+RESOLVED_URL = config("RESOLVED_URL", default="")
+SHARE_URL = config("SHARE_URL", default="")
+
